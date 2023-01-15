9cdc8064c91cf44faeecae0b39c8d47662fe33aa
sinaezar@gmail.com <sinaezar@gmail.com>
2023-01-15 17:06:18 +0330

    LinkedStackString was implemented

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedStackOfStrings strList = new LinkedStackOfStrings();
+        strList.push("hi");
+        strList.push(" there");
+    }
+}
